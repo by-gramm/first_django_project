8a90e6b9f63e2d746cf1bb512cb58d3364c3515e
by-gramm <ready2start@naver.com>
2021-04-12 19:14:31 +0900

    Add sign up function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from accounts import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import SignupForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "회원가입에 성공하셨습니다! 이민기 씨도 기뻐하시겠군요.")
+            return redirect("/")
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
