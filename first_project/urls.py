295b617f3d40b00b888fc584ef658fb6073c61e7
by-gramm <ready2start@naver.com>
2021-04-12 16:24:42 +0900

    Create etc.html template

--- first_project/urls.py
+++ first_project/urls.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 앞선 패턴에 포함되지 않는 모든 경우
+    re_path('', TemplateView.as_view(template_name='etc.html'), name='etc'),
 ]
 
 if settings.DEBUG:
