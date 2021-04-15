6cd525ddb756aeff91be93151d01d6eb232493ee
by-gramm <ready2start@naver.com>
2021-04-15 17:12:13 +0900

    Create 'Post' model

--- happy_birthday/models.py
+++ happy_birthday/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000, blank=False, help_text="1,000자 이내로 작성해 주세요.")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # TODO: 댓글 클래스 만들고 연결하기
