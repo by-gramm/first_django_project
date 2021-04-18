9668c48354176a8c95a5111b612169b20cc21f7c
by-gramm <ready2start@naver.com>
2021-04-18 19:48:54 +0900

    Add introduce page

--- first_project/urls.py
+++ first_project/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('introduce/', TemplateView.as_view(template_name='introduce.html'), name='introduce'),
     path('accounts/', include('accounts.urls')),
     path('happy_birthday/', include('happy_birthday.urls')),
     path('', TemplateView.as_view(template_name='root.html'), name='root'),
