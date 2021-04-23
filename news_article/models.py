522c05573bffe52fa9bb9f1061bf05629dc04bc7
by-gramm <ready2start@naver.com>
2021-04-24 06:39:54 +0900

    Revise 'Article' model & Add several articles

--- news_article/models.py
+++ news_article/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Article(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, blank=False)
-    article_image = models.ImageField(blank=False, upload_to='article_image/%Y/%m/%d')
+    article_image = models.ImageField(blank=True, upload_to='article_image/%Y/%m/%d')
     content = models.TextField(max_length=2000, blank=False, help_text="2,000자 이내로 작성해 주세요.")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
